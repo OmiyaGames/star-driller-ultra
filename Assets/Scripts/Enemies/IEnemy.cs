b63e7495fdd96e56b843b50f835d3965620e3e7f
Taro Omiya <taro.omiya@omiyagames.com>
2015-04-18 13:56:36 -0400

    Adding ram animation.

---
+++ Assets/Scripts/Enemies/IEnemy.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IEnemy : IPooledObject
+{
+    [SerializeField]
+    int maxHealth = 1;
+    [SerializeField]
+    PooledExplosion explosion = null;
+
+    EnemyCollection collection;
+    int currentHealth = 0;
+
+    public int CurrentHealth
+    {
+        get
+        {
+            return currentHealth;
+        }
+        set
+        {
+            int newHealth = Mathf.Clamp(value, 0, maxHealth);
+            if (currentHealth != newHealth)
+            {
+                currentHealth = newHealth;
+                if(currentHealth <= 0)
+                {
+                    Die();
+                }
+            }
+        }
+    }
+
+    public EnemyCollection Collection
+    {
+        get
+        {
+            return collection;
+        }
+        set
+        {
+            collection = value;
+        }
+    }
+
+	// Use this for initialization
+	public override void Start ()
+    {
+        CurrentHealth = maxHealth;
+	}
+
+    protected virtual void Die()
+    {
+        if(explosion != null)
+        {
+            // Create an explosion here
+            Singleton.Get<PoolingManager>().GetInstance(explosion.gameObject, transform.position, transform.rotation);
+        }
+        if (Collection != null)
+        {
+            Collection.RemoveEnemy(this);
+        }
+        gameObject.SetActive(false);
+    }
+
+    public bool MatchEnemy(EnemyCollection.EnemyInfo obj)
+    {
+        return (obj.EnemyScript == this);
+    }
+}
---
+++ Assets/Scripts/Enemies/PooledExplosion.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class PooledExplosion : IPooledObject
+{
+    [SerializeField]
+    float dieAfter = 5f;
+
+    ParticleSystem cache = null;
+
+	// Use this for initialization
+	public override void Start ()
+    {
+        if(cache == null)
+        {
+            cache = GetComponent<ParticleSystem>();
+        }
+        cache.Play();
+        StartCoroutine(Die());
+	}
+
+    IEnumerator Die()
+    {
+        yield return new WaitForSeconds(dieAfter);
+        gameObject.SetActive(false);
+    }
+}
