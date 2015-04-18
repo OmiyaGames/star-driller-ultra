757b707828c9f1ba4d5d4e868dc449b28912486c
Taro Omiya <taro.omiya@omiyagames.com>
2015-04-18 11:06:00 -0400

    Working on adjusting the controls sensitiviy.  Still not working as I'd like, but it's OK so far.

--- Assets/Scripts/ShipControl.cs
+++ Assets/Scripts/ShipControl.cs
@@ -4,23 +4,31 @@ using System.Collections;
 [RequireComponent(typeof(Rigidbody))]
 public class ShipControl : MonoBehaviour
 {
+    public enum FlightMode
+    {
+        ToTheTarget,
+        AwayFromTheTarget
+    }
+
     [SerializeField]
     Transform target = null;
     [SerializeField]
     Transform camera = null;
 
     [SerializeField]
-    [Range(0, 100)]
-    float moveTowardsTarget = 10f;
+    [Range(0, 1000)]
+    float forceTowardsTarget = 100f;
     [SerializeField]
-    [Range(0, 100)]
-    float moveSidewaysSpeed = 10f;
+    [Range(0, 20)]
+    float forceSidewaysSpeed = 10f;
     [SerializeField]
     [Range(0, 30)]
     float rotateLerp = 1f;
 
     Rigidbody bodyCache = null;
     Vector2 controlInput = Vector2.zero;
+    Vector3 targetToShip = Vector3.zero,
+        forceCache = Vector3.zero;
     Quaternion currentRotation = Quaternion.identity;
     Quaternion lookRotation = Quaternion.identity;
 
@@ -43,7 +51,9 @@ public class ShipControl : MonoBehaviour
         controlInput.y = Input.GetAxis("Vertical");
 
         // Figure out the direction to look at
-        lookRotation = Quaternion.LookRotation((target.position - transform.position), camera.up);
+        targetToShip = (target.position - transform.position);
+        targetToShip.Normalize();
+        lookRotation = Quaternion.LookRotation(targetToShip, camera.up);
 
         // Update rotation
         Body.rotation = Quaternion.Lerp(Body.rotation, lookRotation, (Time.deltaTime * rotateLerp));
@@ -51,11 +61,16 @@ public class ShipControl : MonoBehaviour
 
     void FixedUpdate()
     {
-        // Check if we're doing any controls
-        if((Mathf.Approximately(controlInput.x, 0) == false) || (Mathf.Approximately(controlInput.y, 0) == false))
-        {
-            // Apply velocity
-            Body.velocity = lookRotation * (controlInput * moveSidewaysSpeed);
-        }
+        // Add controls force
+        forceCache.x = controlInput.x * forceSidewaysSpeed;
+        forceCache.y = controlInput.y * forceSidewaysSpeed;
+        forceCache.z = 0;
+        Body.AddRelativeForce(forceCache, ForceMode.Impulse);
+
+        // Add forward force
+        forceCache.x = 0;
+        forceCache.y = 0;
+        forceCache.z = forceTowardsTarget;
+        Body.AddRelativeForce(forceCache, ForceMode.Force);
     }
 }
